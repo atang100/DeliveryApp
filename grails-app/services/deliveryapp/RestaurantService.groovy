b29e769f6f2e2a887972e8a8f635e8614c54a6f2
atang100 <atang100@uottawa.ca>
2015-07-17 23:18:10 -0400

    Removed duplicate code inside restaurant service.

--- grails-app/services/deliveryapp/RestaurantService.groovy
+++ grails-app/services/deliveryapp/RestaurantService.groovy
@@ -5,24 +5,4 @@ import grails.transaction.Transactional
 @Transactional
 class RestaurantService {
 
-    def displayRestaurant() {
-        try {
-            ArrayList<Restaurant> restaurantArrayList = Restaurant.findAll()
-            return restaurantArrayList
-        } catch (Exception e) {
-
-        }
-    }
-
-    def getMenuItems(String restaurantName) {
-        try {
-            Restaurant restaurant = Restaurant.findByRestName(restaurantName)
-            ArrayList<MenuItem> menuItems = MenuItem.findAllByRestaurant(restaurant)
-            return restaurant
-        } catch (Exception e) {
-
-        }
-    }
-
-
 }
