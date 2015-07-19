e17c43d83112ebe3a51de3a893c1f38d95880fdd
atang100 <atang100@uottawa.ca>
2015-07-19 03:39:17 -0400

    Implemented get total for shopping cart.

--- grails-app/domain/deliveryapp/ShoppingCart.groovy
+++ grails-app/domain/deliveryapp/ShoppingCart.groovy
@@ -1,5 +1,7 @@
 package deliveryapp
 
+import java.text.DecimalFormat
+
 class ShoppingCart {
 
     double subTotal
@@ -10,5 +12,31 @@ class ShoppingCart {
     static hasMany = [shoppingCartItems: ShoppingCartItem]
 
     static constraints = {
+    }
+
+    public String findSubTotal() {
+        double tempTotal = 0
+        for (item in shoppingCartItems) {
+            tempTotal += item.quantity*item.price
+        }
+        tempTotal += 5 //Delivery Charge
+        subTotal = tempTotal
+
+        DecimalFormat df = new DecimalFormat("#.00")
+        return df.format(tempTotal)
+    }
+
+    public String findTax() {
+        findSubTotal()
+        tax = subTotal*0.13
+        DecimalFormat df = new DecimalFormat("#.00")
+        return df.format(tax)
+    }
+
+    public String Total() {
+        findTax()
+        total = subTotal + tax
+        DecimalFormat df = new DecimalFormat("#.00")
+        return df.format(total)
     }
 }
